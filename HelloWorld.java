630d1c56c8cc67d20514cfb1109a1b77ecbafd83
huudung <dung.nh0035@hsc.edu.vn>
2019-08-04 20:28:00 +0700

    version D

--- HelloWorld.java
+++ HelloWorld.java
@@ -20,6 +20,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, CCC");
+        System.out.println("Hello, DDD");
     }
 }
